c44a5d3e82a994f4967f1e06ef751d35903a51e1
aiXpertLab <aiXpertLab@gmail.com>
2024-04-10 22:40:09 -0400

    az

--- 1.Basic/ChatCompletion.py
+++ 1.Basic/ChatCompletion.py
@@ -3,12 +3,12 @@ from openai import AzureOpenAI
 
 client = AzureOpenAI(
     api_key         =os.environ.get("AZURE_OPENAI_API_KEY"),  
-    api_version     ="2023-12-01-preview",
+    api_version     ="2024-02-01",
     azure_endpoint  =os.environ.get("AZURE_OPENAI_ENDPOINT"),
 )
 
 response = client.chat.completions.create(    
-    model   =   os.environ.get("AZURE_DEPLOYMENT"),
+    model   =   "gpt-35-turbo-0301",
     messages=[
         {"role": "system",      "content": "You are a helpful assistant."},
         {"role": "user",        "content": "Maryland capital?"}
